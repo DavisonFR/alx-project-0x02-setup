fcf9005f0a31d6d88263eba54b622bfa4d64d8b4
Dave <dawitfirew439@gmail.com>
2025-06-06 12:37:53 +0300

    Added /posts link to Header navigation

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav>
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
+    <nav className="bg-blue-600 text-white p-4 flex justify-center space-x-6">
+      <Link href="/home" className="hover:underline">Home</Link>
+      <Link href="/about" className="hover:underline">About</Link>
+      <Link href="/posts" className="hover:underline">Posts</Link>  {/* ✅ Added /posts */}
     </nav>
   );
 }
